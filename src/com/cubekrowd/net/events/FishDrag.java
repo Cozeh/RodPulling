6bade863a9ac03fc7215201a8efaa4c6e6020f7c
Cozeh <hernico4@yahoo.com>
2016-12-31 05:12:57 +0200

    Secondary commit

--- src/com/cubekrowd/net/events/FishDrag.java
+++ src/com/cubekrowd/net/events/FishDrag.java
@@ -8,20 +8,48 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.util.Vector;
 
+import com.cubekrowd.net.Main;
+
 public class FishDrag implements Listener {
 
+	private Main plugin;
+	
+	public FishDrag(Main pl){	
+		plugin = pl;
+	}
+
+	int x = 0;
+	
 	@EventHandler
 	public void onFishDrag(PlayerFishEvent event) {
+		x = plugin.getConfig().getInt("isOn");
+		if((!(x == 0)) || (!(x == 1))){
+			x = 1;
+			plugin.getConfig().set("isOn", x);
+			plugin.saveConfig();
+		}
+		
+		String mult = plugin.getConfig().getString("mult");
+		int res = Integer.parseInt(mult);
+		
+		if(res > 5 || res < 1){
+			res = 2;
+		}
+		
+		plugin.getConfig().set("mult", res);
+		
+		if(x == 1){
+				
 		Player player = event.getPlayer();
 		if (event.getState() == PlayerFishEvent.State.CAUGHT_ENTITY) {
 			Entity entity = event.getCaught();
 			if (entity instanceof LivingEntity) {
 				Vector dir = entity.getLocation().toVector().subtract(player.getLocation().toVector()).normalize();
-				dir.multiply(2);
+				dir.multiply(res);
 				dir.add(new Vector(0,1,0));
 				player.setVelocity(dir);
 			}
 		}
 	}
-
+	}
 }
